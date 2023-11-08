d1d171d2850ff84d6cba8486d0523b2a7e4ee987
RVany <vanmathyrr@gmail.com>
2023-11-07 21:03:31 -0500

    TAPPING ISSUE FOR CREATE COMMUNITY LINK FIXED

--- src/webparts/reactMyGroups/components/reactMyGroups/ReactMyGroups.tsx
+++ src/webparts/reactMyGroups/components/reactMyGroups/ReactMyGroups.tsx
@@ -64,7 +64,7 @@ export class ReactMyGroups extends React.Component<IReactMyGroupsProps, IReactMy
     return (
       <div className={styles.reactMyGroups} style={{ backgroundColor: semanticColors.bodyBackground }}>
         <div className={styles.title} role="heading" aria-level={2}>{(this.strings.userLang == "FR" ? this.props.titleFr :this.props.titleEn )} </div>
-        <div className={styles.seeAll}>{this.props.toggleSeeAll == false && <a aria-label={this.strings.seeAllLabel} href={this.props.seeAllLink}>{this.strings.seeAll}</a>}</div>
+        {this.props.toggleSeeAll == false && <a aria-label={this.strings.seeAllLabel} href={this.props.seeAllLink}><div className={styles.seeAll}>{this.strings.seeAll}</div></a>}
         <div className={styles.createComm}><Icon iconName="Add" className={styles.addIcon} /><a href={this.props.createCommLink}>{this.strings.createComm}</a></div>
           {this.state.isLoading ?
     <Spinner label={this.strings.loadingState} />
