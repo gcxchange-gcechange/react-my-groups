abffddcb1b97fc73b47acdbf09e6343644e4045b
MoonSky1! <vanmathyrr@gmail.com>
2024-03-01 15:24:15 -0500

    grid layout redesign to display more details

--- src/webparts/reactMyGroups/components/reactMyGroups/ReactMyGroups.tsx
+++ src/webparts/reactMyGroups/components/reactMyGroups/ReactMyGroups.tsx
@@ -1,20 +1,22 @@
-import * as React from 'react';
-import styles from './ReactMyGroups.module.scss';
-import { IReactMyGroupsProps } from './IReactMyGroupsProps';
-import GroupService from '../../../../services/GroupService';
-import { IReactMyGroupsState } from './IReactMyGroupsState';
-import { GroupList } from '../GroupList';
-import { Spinner, ISize } from 'office-ui-fabric-react';
-import { GridLayout } from '../GridList';
-import { ListLayout } from '../ListLayout';
+import * as React from "react";
+import styles from "./ReactMyGroups.module.scss";
+import { IReactMyGroupsProps } from "./IReactMyGroupsProps";
+import GroupService from "../../../../services/GroupService";
+import { IReactMyGroupsState } from "./IReactMyGroupsState";
+import { GroupList } from "../GroupList";
+import { Spinner, ISize } from "office-ui-fabric-react";
+import { GridLayout } from "../GridList";
+import { ListLayout } from "../ListLayout";
 //import * as strings from 'ReactMyGroupsWebPartStrings';
-import { IReadonlyTheme } from '@microsoft/sp-component-base';
-import { Icon } from 'office-ui-fabric-react/lib/Icon';
-import { Paging } from '../../components/paging';
-import { SelectLanguage } from '../SelectLanguage';
+import { IReadonlyTheme } from "@microsoft/sp-component-base";
+import { Icon } from "office-ui-fabric-react/lib/Icon";
+import { Paging } from "../../components/paging";
+import { SelectLanguage } from "../SelectLanguage";
 
-export class ReactMyGroups extends React.Component<IReactMyGroupsProps, IReactMyGroupsState> {
-
+export class ReactMyGroups extends React.Component<
+  IReactMyGroupsProps,
+  IReactMyGroupsState
+> {
   constructor(props: IReactMyGroupsProps) {
     super(props);
 
@@ -24,22 +26,30 @@ export class ReactMyGroups extends React.Component<IReactMyGroupsProps, IReactMy
       currentPage: 1,
       pagelimit: 0,
       showless: false,
-      pageSeeAll: false
+      pageSeeAll: false,
     };
   }
 
   public strings = SelectLanguage(this.props.prefLang);
-  public async componentDidUpdate (prevProps:IReactMyGroupsProps):Promise<void>{
+  public async componentDidUpdate(
+    prevProps: IReactMyGroupsProps
+  ): Promise<void> {
     // if (prevProps.prefLang !== this.props.prefLang) {
     //   this.strings = SelectLanguage(this.props.prefLang);
     //    this.props.updateWebPart();
     // }
-  this.props.updateWebPart();
+    this.props.updateWebPart();
   }
 
   public render(): React.ReactElement<IReactMyGroupsProps> {
-    let myData=[];
-    (this.props.sort === "DateCreation") ?  myData = [].concat(this.state.groups).sort((a, b) => a.createdDateTime < b.createdDateTime ? 1 : -1) : myData = [].concat(this.state.groups).sort((a, b) => a.displayName < b.displayName ? 1 : -1);
+    let myData = [];
+    this.props.sort === "DateCreation"
+      ? (myData = []
+          .concat(this.state.groups)
+          .sort((a, b) => (a.createdDateTime < b.createdDateTime ? 1 : -1)))
+      : (myData = []
+          .concat(this.state.groups)
+          .sort((a, b) => (a.displayName < b.displayName ? 1 : -1)));
     let pagedItems: any[] = myData;
     const totalItems: number = pagedItems.length;
 
@@ -47,13 +57,12 @@ export class ReactMyGroups extends React.Component<IReactMyGroupsProps, IReactMy
     const { currentPage } = this.state;
 
     //if on see all page, only show 20 at the time
-    if(this.props.toggleSeeAll){
+    if (this.props.toggleSeeAll) {
       maxEvents = 20;
     }
     if (true && totalItems > 0 && totalItems > maxEvents) {
-
       const pageStartAt: number = maxEvents * (currentPage - 1);
-      const pageEndAt: number = (maxEvents * currentPage);
+      const pageEndAt: number = maxEvents * currentPage;
 
       pagedItems = pagedItems.slice(pageStartAt, pageEndAt);
     }
@@ -61,100 +70,152 @@ export class ReactMyGroups extends React.Component<IReactMyGroupsProps, IReactMy
     const { semanticColors }: IReadonlyTheme = this.props.themeVariant;
 
     return (
-      <div className={styles.reactMyGroups} style={{ backgroundColor: semanticColors.bodyBackground }}>
-        <div className={styles.title} role="heading" aria-level={2}>{(this.strings.userLang === "FR" ? this.props.titleFr :this.props.titleEn )} </div>
-        {(this.props.toggleSeeAll === false && !!this.props.seeAllLink) && <a aria-label={this.strings.seeAllLabel} href={this.props.seeAllLink}><div className={styles.seeAll}>{this.strings.seeAll}</div></a>}
-        <div className={styles.createComm}><Icon iconName="Add" className={styles.addIcon} /><a href={this.props.createCommLink}>{this.strings.createComm}</a></div>
-          {this.state.isLoading ?
-    <Spinner label={this.strings.loadingState} />
-    :
-    <div>
-    <div className={styles.groupsContainer}>
-      {this.props.layout === 'Compact' ?
-        <GroupList groups={pagedItems} onRenderItem={(item: any, index: number) => this._onRenderItem(item, index)}/>
-      :
-        this.props.layout === 'Grid' ?
-        <GridLayout sort={this.props.sort} items={pagedItems} onRenderGridItem={(item: any, finalSize: ISize, isCompact: boolean) => this._onRenderGridItem(item, finalSize, isCompact)}/>
-        :
-          <ListLayout sort={this.props.sort} items={pagedItems} onRenderListItem={(item: any, finalSize: ISize, isCompact: boolean) => this._onRenderListItem(item, finalSize, isCompact)}/>
-      }
-                  {this.props.toggleSeeAll ?
-                  <div>
-                    <Paging
-                      showPageNumber={true}
-                      currentPage={currentPage}
-                      itemsCountPerPage={20}
-                      totalItems={totalItems}
-                      onPageUpdate={this._onPageUpdate}
-                      nextButtonLabel={this.strings.pagNext}
-                      previousButtonLabel={this.strings.pagPrev}
-                    />
-                  </div> : ""
+      <div
+        className={styles.reactMyGroups}
+        style={{ backgroundColor: semanticColors.bodyBackground }}
+      >
+        <div className={styles.title} role="heading" aria-level={2}>
+          {this.strings.userLang === "FR"
+            ? this.props.titleFr
+            : this.props.titleEn}{" "}
+        </div>
+        {this.props.toggleSeeAll === false && !!this.props.seeAllLink && (
+          <a aria-label={this.strings.seeAllLabel} href={this.props.seeAllLink}>
+            <div className={styles.seeAll}>{this.strings.seeAll}</div>
+          </a>
+        )}
+        <div className={styles.createComm}>
+          <Icon iconName="Add" className={styles.addIcon} />
+          <a href={this.props.createCommLink}>{this.strings.createComm}</a>
+        </div>
+        {this.state.isLoading ? (
+          <Spinner label={this.strings.loadingState} />
+        ) : (
+          <div>
+            <div className={styles.groupsContainer}>
+              {this.props.layout === "Compact" ? (
+                <GroupList
+                  groups={pagedItems}
+                  onRenderItem={(item: any, index: number) =>
+                    this._onRenderItem(item, index)
                   }
+                />
+              ) : this.props.layout === "Grid" ? (
+                <GridLayout
+                  sort={this.props.sort}
+                  items={pagedItems}
+                  onRenderGridItem={(
+                    item: any,
+                    finalSize: ISize,
+                    isCompact: boolean
+                  ) => this._onRenderGridItem(item, finalSize, isCompact)}
+                />
+              ) : (
+                <ListLayout
+                  sort={this.props.sort}
+                  items={pagedItems}
+                  onRenderListItem={(
+                    item: any,
+                    finalSize: ISize,
+                    isCompact: boolean
+                  ) => this._onRenderListItem(item, finalSize, isCompact)}
+                />
+              )}
+              {this.props.toggleSeeAll ? (
+                <div>
+                  <Paging
+                    showPageNumber={true}
+                    currentPage={currentPage}
+                    itemsCountPerPage={20}
+                    totalItems={totalItems}
+                    onPageUpdate={this._onPageUpdate}
+                    nextButtonLabel={this.strings.pagNext}
+                    previousButtonLabel={this.strings.pagPrev}
+                  />
                 </div>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+        )}
       </div>
-
-          }
-      </div>
-
     );
   }
 
-  public componentDidMount (): void {
+  public componentDidMount(): void {
     this._getGroups();
     this.setState({
-      pagelimit: this.props.numberPerPage
+      pagelimit: this.props.numberPerPage,
     });
   }
 
   public _getGroups = (): void => {
-    GroupService.getGroups().then(groups => {
+    GroupService.getGroups().then((groups) => {
       this.setState({
-        groups: groups
+        groups: groups,
       });
 
       this._getGroupLinks(groups);
     });
-  }
+  };
 
   public _getGroupLinks = (groups: any): void => {
-    GroupService.getGroupLinksBatch(groups).then(groupUrls => {
-      this.setState(prevState => ({
-        groups: prevState.groups.map(group => group.id !== null ? {...group, url: groupUrls[group.id]} : group)
+    GroupService.getGroupLinksBatch(groups).then((groupUrls) => {
+      this.setState((prevState) => ({
+        groups: prevState.groups.map((group) =>
+          group.id !== null ? { ...group, url: groupUrls[group.id] } : group
+        ),
       }));
     });
 
     this._getGroupMembers(groups);
-  }
+  };
 
   public _getGroupMembers = (groups: any): void => {
-    GroupService.getGroupMembersBatch(groups).then(groupMembers => {
-      this.setState(prevState => ({
-        groups: prevState.groups.map(group => group.id !== null ? {...group, members: groupMembers[group.id]} : group)
+    GroupService.getGroupMembersBatch(groups).then((groupMembers) => {
+      this.setState((prevState) => ({
+        groups: prevState.groups.map((group) =>
+          group.id !== null
+            ? { ...group, members: groupMembers[group.id] }
+            : group
+        ),
       }));
     });
 
     this._getGroupThumbnails(groups);
-  }
+  };
 
   public _getGroupThumbnails = (groups: any): void => {
-    GroupService.getGroupThumbnailsBatch(groups).then(grouptbs => {
-      this.setState(prevState => ({
-        groups: prevState.groups.map(group => group.id !== null ? {...group, thumbnail: "data:image/jpeg;base64," + grouptbs[group.id], color: "#0078d4"} : group)
+    GroupService.getGroupThumbnailsBatch(groups).then((grouptbs) => {
+      this.setState((prevState) => ({
+        groups: prevState.groups.map((group) =>
+          group.id !== null
+            ? {
+                ...group,
+                thumbnail: "data:image/jpeg;base64," + grouptbs[group.id],
+                color: "#0078d4",
+              }
+            : group
+        ),
       }));
     });
 
     this.setState({
-      isLoading: false
+      isLoading: false,
     });
-  }
+  };
 
   private _onRenderItem = (item: any, index: number): JSX.Element => {
     return (
       <div className={styles.compactContainer}>
         <a className={styles.compactA} href={item.url}>
           <div className={styles.compactWrapper}>
-            <img className={styles.compactBanner} src={item.thumbnail} alt={`${this.strings.altImgLogo} ${item.displayName}`}/>
+            <img
+              className={styles.compactBanner}
+              src={item.thumbnail}
+              alt={`${this.strings.altImgLogo} ${item.displayName}`}
+            />
             <div className={styles.compactDetails}>
               <div className={styles.compactTitle}>{item.displayName}</div>
             </div>
@@ -162,53 +223,149 @@ export class ReactMyGroups extends React.Component<IReactMyGroupsProps, IReactMy
         </a>
       </div>
     );
-  }
+  };
 
-  private _onRenderGridItem = (item: any, finalSize: ISize, isCompact: boolean): JSX.Element => {
-
+  private _onRenderGridItem = (
+    item: any,
+    finalSize: ISize,
+    isCompact: boolean
+  ): JSX.Element => {
     return (
       <div className={styles.siteCard}>
-      <a href={item.url}>
-        <div className={styles.cardBanner}>
-          <div className={styles.topBanner} style={{backgroundColor: item.color}} />
-          <img className={styles.bannerImg} src={item.thumbnail} alt={`${this.strings.altImgLogo} ${item.displayName}`} />
-          <h3 className={styles.cardTitle}>{item.displayName}</h3>
-          <p
-          className={styles.cardDescription}
-          aria-label={item.description}
-        >
-          {item.description}
-        </p>
-        </div>
-      </a>
-    </div>
-    );
-  }
-
-  private _onRenderListItem = (item: any, finalSize: ISize, isCompact: boolean): JSX.Element => {
-
-    return (
-        <div className={styles.siteCardList}>
-        <a className="community-list-item" href={item.url}>
-           <div className={styles.cardBannerList}>
-                <div className={styles.articleFlex} style={{'width':'60px'}}>
-                   <img className={styles.bannerImgList} src={item.thumbnail} alt={`${this.strings.altImgLogo} ${item.displayName}`} />
-                </div>
-                <div className={`${styles.articleFlex} ${styles.secondSection}`}>
-                  <div className={styles.cardTitleList}>{item.displayName}</div>
-                  <div className={styles.cardDescription}>{item.description}</div>
-                <div className={styles.members}>{item.members} {this.strings.members}</div>
-              </div>
-              </div>
-            </a>
+        <a href={item.url}>
+          <div className={styles.cardBanner}>
+            <div
+              className={styles.topBanner}
+              style={{ backgroundColor: item.color }}
+            />
+            <img
+              className={styles.bannerImg}
+              src={item.thumbnail}
+              alt={`${this.strings.altImgLogo} ${item.displayName}`}
+            />
           </div>
-          );
-        }
+          <div className={styles.gridContent}>
+            <div className={styles.mainContent}>
+              <h3 className={styles.cardTitle}>{item.displayName}</h3>
+              <p
+                className={styles.cardDescription}
+                aria-label={item.description}
+              >
+                {item.description}
+              </p>
+            </div>
+            <div className={styles.footerContent}>
+              <div className={styles.footerColumn1}>
+                <p aria-label={this.strings.members}>
+                  <span
+                    style={{
+                      fontWeight: "700",
+                      fontSize: "12px",
+                      paddingRight: "5px",
+                    }}
+                  >
+                    {this.strings.members} : 
+                  </span>
+                  <span>{item.members}</span>
+                </p>
 
+                <p aria-label={this.strings.siteViews}>
+                  <span
+                    style={{
+                      fontWeight: "700",
+                      fontSize: "12px",
+                      paddingRight: "5px",
+                    }}
+                  >
+                    {this.strings.siteViews} : 
+                  </span>
+                  <span>8 {item.views}</span>
+                </p>
+              </div>
+              <div className={styles.footerColumn2}>
+                <p aria-label={this.strings.created}>
+                  <span
+                    style={{
+                      fontWeight: "700",
+                      fontSize: "12px",
+                      paddingRight: "5px",
+                    }}
+                  >
+                    {this.strings.created} : 
+                  </span>
+                  <span>
+                    {this.strings.userLang === "fr-fr"
+                      ? new Date(item.createdDateTime).toLocaleDateString(
+                          "fr-CA"
+                        )
+                      : new Date(item.createdDateTime).toLocaleDateString(
+                          "en-CA"
+                        )}
+                  </span>
+                </p>
 
-   private _onPageUpdate = (pageNumber: number): void => {
+                <p aria-label={this.strings.lastModified}>
+                  <span
+                    style={{
+                      fontWeight: "700",
+                      fontSize: "12px",
+                      paddingRight: "5px",
+                    }}
+                  >
+                    {this.strings.lastModified} : 
+                  </span>
+                  <span
+                    aria-label={
+                      this.strings.userLang === "fr-fr"
+                        ? new Date(item.modified).toLocaleDateString("fr-CA")
+                        : new Date(item.modified).toLocaleDateString("en-CA")
+                    }
+                  >
+                    {this.strings.userLang === "fr-fr"
+                      ? new Date(item.modified).toLocaleDateString("fr-CA")
+                      : new Date(item.modified).toLocaleDateString("en-CA")}
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    );
+  };
+
+  private _onRenderListItem = (
+    item: any,
+    finalSize: ISize,
+    isCompact: boolean
+  ): JSX.Element => {
+    return (
+      <div className={styles.siteCardList}>
+        <a className="community-list-item" href={item.url}>
+          <div className={styles.cardBannerList}>
+            <div className={styles.articleFlex} style={{ width: "60px" }}>
+              <img
+                className={styles.bannerImgList}
+                src={item.thumbnail}
+                alt={`${this.strings.altImgLogo} ${item.displayName}`}
+              />
+            </div>
+            <div className={`${styles.articleFlex} ${styles.secondSection}`}>
+              <div className={styles.cardTitleList}>{item.displayName}</div>
+              <div className={styles.cardDescription}>{item.description}</div>
+              <div className={styles.members}>
+                {item.members} {this.strings.members}
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    );
+  };
+
+  private _onPageUpdate = (pageNumber: number): void => {
     this.setState({
-      currentPage: pageNumber
+      currentPage: pageNumber,
     });
-  }
+  };
 }
